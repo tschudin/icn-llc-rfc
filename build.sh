40b8d3fd6e40b2a5cf17e7165f8d8cfee7d2fedd
Christopher Wood <christopherwood07@gmail.com>
2015-08-28 11:35:20 -0700

    build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+name=$1
+prefix=${name%.*}
+echo "Converting ${prefix}.mkd to XML"
+kramdown-rfc2629 $1 > $prefix.xml
+echo "Converting ${prefix}.xml to RFC text"
+xml2rfc --text $prefix.xml
+rm $prefix.xml
+ 
