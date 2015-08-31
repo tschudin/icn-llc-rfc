ba7d0908e2221d3bbe9689b69b6257c93d42106d
Christopher Wood <christopherwood07@gmail.com>
2015-08-31 16:06:04 -0700

    Updated the draft and build script -- distributing now

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ echo "Converting ${prefix}.mkd to XML"
 kramdown-rfc2629 $1 > $prefix.xml
 echo "Converting ${prefix}.xml to RFC text"
 xml2rfc --text $prefix.xml
+xml2rfc --html $prefix.xml
 rm $prefix.xml
  
